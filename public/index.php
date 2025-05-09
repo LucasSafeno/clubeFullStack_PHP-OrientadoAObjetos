c532453cc9f3489be94010b8ec2d032abc346264
Lucas Safeno <lugnorio@gmail.com>
2025-05-09 18:11:45 -0300

    PHP orientado a Objetos#05 - Objeto padrão do PHP

--- public/index.php
+++ public/index.php
@@ -2,39 +2,9 @@
 // require "../helpers/autoload.php";
 require "../vendor/autoload.php";
 
-use app\classes\Crud;
-use app\classes\Login;
 
-$login = new Login();
-$crud = new Crud();
+// $user = ['name' => 'Lucas User', 'email' => 'lucas@email.com'];
 
-//? Atribuindo valor a propriedade
-$login->email = "lucas@email.com";
-$login->password = "123";
-
-//var_dump($login);
-
-echo helper();
-
-echo "<br>";
-echo "<br>";
-
-//? chamando a propriedade
-// echo $login->email;
-
-//? Chamando metodo
-//echo $login->auth();
-echo $crud->read();
-
-//? Transformar em obj
-$user = ['name' => 'Lucas User', 'email' => 'lucas@email.com'];
-
-$userObj = (Object) $user;
-
-var_dump($userObj);
-
-echo $userObj->name;
-
-
-
-;
+$stdClass = new stdClass;
+$stdClass->name = 'Lucas';
+var_dump($stdClass);
