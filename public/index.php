50583f7161f9b186f541a0b630f50a6869bc1b60
Lucas Safeno <lugnorio@gmail.com>
2025-05-12 10:13:56 -0300

    PHP Orientado a Objetos#13 - Construct na classe pai e filha

--- public/index.php
+++ public/index.php
@@ -2,9 +2,28 @@
 
 require "../vendor/autoload.php";
 
-use app\classes\User;
-use app\classes\Person;
+class User
+{
 
+    public function __construct(public string $name, public int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    } // construct
 
-$user = new User;
-echo $user->info();
+    public function getUserInfo()
+    {
+        return $this->name . " : " . $this->age;
+    }
+}//? User
+
+class User2 extends User
+{
+    public function __construct(string $name, int $age)
+    {
+        parent::__construct($name, $age);
+    }// construct
+}//? User2
+
+$user = new User2('Lucas', '30');
+echo $user->getUserInfo();
